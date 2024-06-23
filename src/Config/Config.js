0d2533cbb28ca7b9728e603203c88af01198f3f0
BazilSuhail <bazil1854@gmail.com>
2024-06-24 00:23:39 +0500

    ...

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -3,17 +3,6 @@ import { useEffect, useState } from "react";
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/storage'; 
-/*
-const firebaseConfig = {
-    apiKey: "AIzaSyAZl78ABlOT-p29czm7pc79AiUcYssqKYI",
-    authDomain: "itu-cms.firebaseapp.com",
-    projectId: "itu-cms",
-    storageBucket: "itu-cms.appspot.com",
-    messagingSenderId: "442793159184",
-    appId: "1:442793159184:web:e121dacdbf26e5805bf895",
-    measurementId: "G-X66E0GNGH4"
-}; 
-*/
   
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
